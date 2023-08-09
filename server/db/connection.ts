20cc1d63913ebc047861de6feae7ca32914bc8c9
Gerard Paapu <gerard@gerardpaapu.com>
2023-08-10 10:16:07 +1200

    fix: rewrite knex boilerplate to work with vitest
    Previously we created our connections like this:
    
    ```ts
    import knex from 'knex'
    const connection = knex.default(config)
    ```
    
    This strategy made tsc, tsx and esbuild happy, but
    made vitest very sad.
    
    So we're back to "write the js we want and tell tsc to be quiet"
    
    ```ts
    import knex, { Knex } from 'knex'
    
    // @ts-expect-error https://github.com/knex/knex/issues/5358
    const connection: Knex = knex(config)
    ```
    
    In JavaScript files, we're back to just doing the normie thing.
    
    ```js
    import knex from 'knex'
    
    const connection = knex(config)
    ```
    
    Since the types will probably not infer correctly, we could add
    a JSDoc notation like this, but I don't know if it's worth it right now.
    
    ```js
    /** @type {import('knex').Knex} */
    const connection = knex(config)
    ```

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,8 +1,10 @@
-import knex from 'knex'
+import knex, { Knex } from 'knex'
 import config from './knexfile.js'
 
-type Environment = 'production'| 'development' | 'test'
+type Environment = 'production' | 'development' | 'test'
 const env = (process.env.NODE_ENV || 'development') as Environment
-const connection = knex.default(config[env])
+
+// @ts-expect-error https://github.com/knex/knex/issues/5358
+const connection: Knex = knex(config[env])
 
 export default connection
