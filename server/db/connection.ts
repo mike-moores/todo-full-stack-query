dab62d873c14421c80ff5862c9766a07697b267e
Gerard Paapu <gerard@gerardpaapu.com>
2023-08-11 09:46:07 +1200

    feat: switch to Bundler resolution
    typescript has a new moduleResolution option "Bundler"
    that is a bit looser, this means we do not have to do anything
    special for knex to import normally and work with
    esbuild, tsx and vitest

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,10 +1,9 @@
-import knex, { Knex } from 'knex'
+import knex from 'knex'
 import config from './knexfile.js'
 
 type Environment = 'production' | 'development' | 'test'
 const env = (process.env.NODE_ENV || 'development') as Environment
 
-// @ts-expect-error https://github.com/knex/knex/issues/5358
-const connection: Knex = knex(config[env])
+const connection = knex(config[env])
 
 export default connection
