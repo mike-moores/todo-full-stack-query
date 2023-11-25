79fe5fd72a5d7390801b41ef32da02e9a94bcb27
mike moores <michaelmoores@hotmail.com>
2023-11-25 16:41:31 +1300

    building the backend API

---
+++ server/db/migrations/20231125030912_task.ts.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary()
+    table.string('details')
+    table.string('priority').defaultTo('Low')
+    table.boolean('completed').defaultTo(false)
+  })
+
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('tasks')
+}
